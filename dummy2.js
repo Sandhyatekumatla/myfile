d3c2ae176643aa7c7cddfa00d592b6dac8226fc9
sandhyatekumatla <sandhyatekumatla543@gmail.com>
2023-11-11 15:16:42 +0530

    one

---
+++ dummy2.js
@@ -0,0 +1,126 @@
+
+
+
+// for (let i=0; i<20; i++)
+// {
+//     if(i>10){
+// break;
+//     }
+//     console.log(i);
+// }
+
+
+//Function scope
+// function fn()
+// {
+//     let a=19;
+//     console.log(a)
+// }
+
+
+// function fn()
+// {
+//     let a=10;
+//     console.log(a);
+
+// }
+// fn()
+// console.log("b")
+//10
+// b
+
+
+// let a=10;
+// function fn()
+// {
+    
+//     console.log(a);
+
+// }
+// fn()
+// console.log("a is letter"+a);
+// // 10
+// // a is letter a
+
+// function fn()
+// {
+//      a=5;
+//     console.log("function" + a);
+// }
+//  function fnn()
+// {
+//     let a=10;
+//     console.log(a)
+// }
+// fn()
+// fnn()
+// console.log("out side fun"+a)
+// //function5
+// //10
+// //out side fun5
+
+
+// function first(a)
+// {
+//     console.log("flowers"+a)
+// }
+// first("love")
+// first("eng")
+// //flowerslove
+// //flowerseng
+
+// function first(a)
+// {
+//     console.log("this is a function"+a);
+// }
+// first ("call")
+// //this is a functioncall
+
+
+// alert ("hii");
+// console.log("hello this is js")
+// //alert hii
+// //hello this is js
+
+
+// var a ="welcome to js";
+// prompt(a);
+// console.log("hello");
+// //hello
+
+// var a="welcome";
+// var name=prompt(a);
+// var b = confirm ("hii");
+// console.log(name);
+// console.log("b")
+// console.log("hello" + name+"this is js");
+// //hr
+// //b
+// //hellohrthis is js
+
+//objects......
+// var person ={name:"sandy", age:20,favcolor:"pink"};
+// console.log(typeof(name));
+// console.log(person.age);
+// console.log(person.favcolor);
+// //string
+// //20
+// //pink
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
