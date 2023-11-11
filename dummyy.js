c447d2e92a92df6ee0b994571af486a2654fcc2d
sandhyatekumatla <sandhyatekumatla543@gmail.com>
2023-11-11 15:14:18 +0530

    file

---
+++ dummyy.js
@@ -0,0 +1,481 @@
+
+//console.log("hello");
+ 
+
+
+// var a=+"6";  
+// console.log(a+2);
+
+// 
+  
+
+
+// function countVowels(str) {
+//     // Convert the string to lowercase to handle both uppercase and lowercase characters
+//     str = str.toLowerCase();
+  
+//     // Initialize a counter for vowels
+//     let vowelCount = 0;
+  
+//     // Define a string containing all vowels
+//     const vowels = "aeiou";
+  
+//     // Loop through each character in the input string
+//     for (let i = 0; i < str.length; i++) {
+//       const char = str[i];
+  
+//       // Check if the character is a vowel
+//       if (vowels.includes(char)) {
+//         vowelCount++;
+//       }
+//     }
+  
+//     // Return the count of vowels
+//     return vowelCount;
+//   }
+  
+//   const inputString = "Hello, World!";
+//   const vowelCount = countVowels(inputString);
+  
+//   console.log("Number of vowels:", vowelCount);
+
+
+
+// to find the number of vowels in string in JS?......
+
+// let str="Hello,Iam Sandhya."
+// let count=0;
+// const vowels=["a","e","i","o","u"]
+
+// for(let a of str.toLowerCase()){
+//     if(vowels.includes(a)){
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+// let str="iam ram";
+// let count=0;
+// const vowels=["a","e","i","o","u"];
+// for (let s of str.toLowerCase()){
+//     if (vowels.includes(s)){
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+//function to count vowels and consonants in a given name
+
+
+
+
+
+// var vowels="sandhya";
+// var index=vowels.indexOf("d");
+// console.log(index);
+
+
+
+// function countConsonants(str) {
+//     // Convert the string to lowercase to handle both uppercase and lowercase characters
+//     str = str.toLowerCase();
+  
+//     // Initialize a counter for consonants
+//     let consonantCount = 0;
+  
+//     // Define a string containing all consonants
+//     const consonants = "bcdfghjklmnpqrstvwxyz";
+  
+//     // Loop through each character in the input string
+//     for (let i = 0; i < str.length; i++) {
+//       const char = str[i];
+  
+//       // Check if the character is a consonant
+//       if (consonants.includes(char)) {
+//         consonantCount++;
+//       }
+//     }
+  
+//     // Return the count of consonants
+//     return consonantCount;
+//   }
+  
+//   const inputString = "Sandy";
+//   const consonantCount = countConsonants(inputString);
+  
+//   console.log("Number of consonants:", consonantCount);
+  
+  
+// function countConsonants(str) {
+//     str=str.toLowerCase();
+//     let consonantCount=0;
+//     const Consonants="bcdfghjklmnpqrstvwxyz";
+//     for (let i=0; i<str.length; i++) {
+//         const char=str[i];
+//         if (Consonants.includes(char)){
+//             consonantCount++;
+//         }
+//         }
+//         return consonantCount;
+//     }const inputString="sandy";
+//     const consonantCount=countConsonants(inputString);
+//     console.log("Number of consonants:", consonantCount);
+    
+
+// a='sandhya';
+// console.log(a);
+
+// a="sandhya";
+// b="shameela";
+// console.log(a,b);
+
+// a="sandhya";
+// b="shameela";
+// console.log(a,b);
+
+
+// a=["sandhya","shameela"];
+// console.log(a+b);
+
+// a='sandhya';
+// a="sham";
+// console.log(a);
+
+// a=["sandhya", "karthi"];
+// console.log(a[3]);
+
+// a={
+//     "name":"sandhya",
+//     age:10,
+//     class:5,
+    
+// }
+// console.log(a);
+
+
+//  a=[{
+//   "name_key":"sandhya",
+//    age:20,
+//  }]
+//  console.log(a[0])
+
+//  var a=10;
+// a=12;
+//  console.log(a,a);
+
+
+// const a=10;
+// b=20;
+// console.log(b,a);
+
+// let a=10;
+// a=20;
+// console.log(a)
+
+
+// let a="san";
+// a="karti";
+// console.log(a);
+
+// var a=10;
+// b="san";
+// console.log(typeof (a));
+// console.log(typeof(b))
+// console.log(typeof(a,b));
+
+// var a=10;
+// console.log(a++);
+// console.log(a)
+
+// var a=10;
+//  console.log(++a);
+//  console.log(a)
+
+// var a=10;
+// console.log(--a);
+// console.log(a)
+
+// var a=10;
+// console.log(a--);
+// console.log(a)
+
+// var a=1;
+// a+=5;
+// console.log(a);
+
+// var a=1;
+// a=+5;
+// console.log(a);
+
+// var a=1;
+// a-=5;
+// console.log(a);
+
+// var a=2;
+// a*=5;
+// console.log(a);
+
+// var a=2;
+// a/=5;
+// console.log(a);
+
+
+// var a=2;
+// a%=2;
+// console.log(a);
+
+
+// var a=2;
+//  a**=2;
+//   console.log(a);
+
+
+// var a=10;
+// var a=10;
+// console.log(a==b);
+
+
+// var a=5;
+// var b=5;
+// console.log(a===b);
+
+// var a=2;
+// var b=5;
+// console.log(a<b);
+
+
+// var a=2;
+// var b=5;
+// console.log(a!=b);
+
+// var a=2;
+// var b=2;
+// console.log(a!==b);
+
+// var a=2;
+// var b=5;
+// console.log(a>=b);
+
+
+// var a=2;
+// var b=5;
+// console.log(a<=b);
+
+// var a=10;
+// var b=10;
+// console.log(a===b && a==b);
+
+// var x=10;
+// var y=10;
+// console.log(x<1 && y>1);
+
+// var x=10;
+// var y=10;
+// console.log(x>20 || y>1);
+
+// var x=10;
+// var y=10;
+// console.log(x==1 || y<1);
+
+
+// var x=10;
+// var y=20;
+// console.log(x!==y || y<1);
+
+
+// var a=2;
+// var b=3;
+// console.log(!true);
+
+// var a=2;
+// var b=3;
+// console.log(!false);
+
+// var a=2;
+//  var b=3;
+//  console.log(typeof(a));
+
+// var a=2;
+//  var b=3;
+//  console.log(typeof(a));
+
+//  var a=true;
+//  console.log(typeof(a));
+
+// var a=5;
+//  b=10;
+//  a<b?console.log("a is smaller"):console.log("a is big");
+
+// let  a=50;
+//    let  b=100;
+//  var c;
+//  var d;
+// console.log(a<b? c=a:d=b);
+
+ 
+// let  a=50;
+//    let  b=100;
+//   var c;
+//   var d;
+//  console.log(a>b? c=a : d=b);
+
+
+// a=10;
+// b=5;
+// console.log(a%2==0?"a is even":"b is odd");
+// console.log(b%2==0?"a is even":"b is odd");
+
+
+//typeconversions.....
+// var a='3'+1;
+// console.log(a);
+
+// let a="2"+2;
+// console.log(a);
+
+// var a='4'-false;
+// console.log(a);
+
+// var a='4'-true;
+//  console.log(a);
+
+// var a="hi"-"hello";
+// console.log(a)
+
+// let result="345";
+// console.log(result);
+
+// var a="453";
+// console.log(a);
+// console.log(typeof(a));
+
+// var a="karthi";
+// console.log(a);
+// console.log(typeof(a));
+
+
+//  var a=Boolean(2);
+// console.log(a);
+
+// var a="143";
+// console.log(a);
+// console.log(typeof(a));
+
+// var str=Boolean(2);
+// console.log(str);
+// console.log(typeof(str));
+
+// var a=10.4;
+// console.log(typeof(a));
+// console.log(a);
+// b=parseInt(a);
+// console.log(b);
+// console.log(typeof(b));
+
+
+// let a=15.55;
+// console.log(a);
+// console.log(typeof(a));
+// console.log(parseInt(a));
+// console.log(parseFloat(a));
+// console.log(Math.floor(a));
+
+// a='hi this is "sandhya"';
+// console.log(a);
+
+// a='hi this is "sandhya"';
+// console.log(a);
+
+// let a="sandhya";
+// console.log(a);
+// console.log(a.length);
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.indexOf("y"));
+// console.log(a.charAt(5));
+// console.log(a.toString());
+
+// console.log('it\'s me')
+// console.log('it\"s me')
+// console.log('it\\me')
+// console.log('it\n me')
+// console.log('it\t me')
+
+
+// console.log('it\'s me')
+// console.log('it\"s me')
+// console.log('it\s me')
+// console.log('it\t me')
+// console.log('it\n me')
+
+// console.log('it\'s you')
+// console.log('it\"s me') 
+// console.log('it\'s \\ me')
+// console.log('it\'s \t me')
+// console.log('it\'s \n me')
+
+//strings...
+//String methods.....
+
+//1).length
+// var myString="san dh ,ya!";
+// var Length=myString.length;
+// console.log(Length);
+
+// let mystring="sandhya";
+// let mynumber=mystring.length;
+// console.log(mynumber);
+
+//2).Concat()
+// const greeting="hello,"
+// const  name="sandhya"
+// const message= greeting.concat(name,"! Welcome to club");
+// console.log(message);
+
+// var mystring="hello,";
+//  mystring+= "world!";
+// console.log(mystring);
+
+// const greeting="good morning " ;
+// const  name="sandy.";
+// const message=greeting. concat( name , " How are you?");
+// console.log(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
